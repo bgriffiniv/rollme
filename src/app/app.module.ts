d5ae6cd05f16d32f3adbe0a599183e72bdf7cafe
bgriffiniv <bgriffiniv@gmail.com>
2017-05-29 17:31:34 -0400

    First shot at an Ionic Creator design.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { Page1Page } from '../pages/page1/page1';
+import { ContactPage } from '../pages/contact/contact';
+import { Page4Page } from '../pages/page4/page4';
+import { Page6Page } from '../pages/page6/page6';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -12,18 +14,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    Page1Page,
+    ContactPage,
+    Page4Page,
+    Page6Page
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    Page1Page,
+    ContactPage,
+    Page4Page,
+    Page6Page
   ],
   providers: [
     StatusBar,
