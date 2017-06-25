46dc78a2294bf2f7a4f14a5c92ecb641e73cc0f3
bgriffiniv <bgriffiniv@gmail.com>
2017-06-24 21:04:32 -0400

    Added external modules
    next step: creating a form using Ionic v2 format; read through tutorial

--- src/pages/rolodex/rolodex.ts
+++ src/pages/rolodex/rolodex.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PersonOnePage } from '../person-one/person-one';
 import { PersonTwoPage } from '../person-two/person-two';
+import {sum} from 'lodash'
+import {schemaForm} from 'angular-schema-form'
 
 @Component({
   selector: 'page-rolodex',
@@ -10,6 +12,8 @@ import { PersonTwoPage } from '../person-two/person-two';
 export class RolodexPage {
 
   constructor(public navCtrl: NavController) {
+        console.log(sum)
+        console.log(schemaForm)
   }
   goToPersonOne(params){
     if (!params) params = {};
