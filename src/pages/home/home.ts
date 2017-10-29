07b541e41e0e990b8be54f87df0b72ac76ceae27
bgriffiniv <bgriffiniv@gmail.com>
2017-10-29 16:36:38 -0400

    Adding data to profile page; Breaking profile page out to separate file;

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,7 @@ import { NavController } from 'ionic-angular';
 
 export class HomePage {
   name: any;
-  users: any;
+  users: Array<{ name: String, company: String, role: String }>;
   //aboutPage = AboutPage;
 
   constructor(public navCtrl: NavController){
@@ -36,23 +36,30 @@ export class HomePage {
       }
     ];
   }
-  goToOtherPage() {
+  goToOtherPage(user) {
     //push another page onto the history stack
     //causing the nav controller to animate the new page in
-    this.navCtrl.push(OtherPage);
+    this.navCtrl.push(OtherPage, {user: user});
   }
+}
 
+import { NavParams } from 'ionic-angular';
+
+@Component({
+  template: `
+  <ion-header>
+    <ion-navbar>
+      <ion-title>Other Page</ion-title>
+    </ion-navbar>
+  </ion-header>
+
+  <ion-content>{{user.name}}</ion-content>`
+})
+export class OtherPage {
+  user: { name: String, company: String, role: String };
+    constructor(private navParams: NavParams){
+      this.user = navParams.get('user');
+    }
 
 
 }
-  @Component({
-    template: `
-    <ion-header>
-      <ion-navbar>
-        <ion-title>Other Page</ion-title>
-      </ion-navbar>
-    </ion-header>
-
-    <ion-content>I'm the other page!</ion-content>`
-  })
- export class OtherPage {}
