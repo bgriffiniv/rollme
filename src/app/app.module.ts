8a28ca5f7b3a3f5dea5ec1a79ae81d3368cd555e
bgriffiniv <bgriffiniv@gmail.com>
2018-02-15 18:13:34 -0500

    Added Firebase credentials;Testing login and CRUD functions next;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule, platformBrowserDynamic } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
