446593e3a1d07169509ae6f83af80bb8bda62407
Brandon Lee <brandxlee11@outlook.com>
2018-10-06 15:11:07 -0400

    Added jquery for login/signup page flow

--- src/pages/splash/splash.ts
+++ src/pages/splash/splash.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, MenuController, NavParams, NavController, ViewController } from 'ionic-angular';
 import { LottieAnimationViewModule } from 'ng-lottie';
+import $ from "jquery";
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../login/login';
+import { HomePage } from '../home/home';
 
 import { DataProvider } from '../../providers/data/data';
 
@@ -17,6 +19,7 @@ export class Splash {
       private anim: any;
       private animationSpeed: number = 1;
 
+
   constructor(public navCtrl: NavController, menuCtrl: MenuController, data: DataProvider, navParams: NavParams,
       public splashScreen: SplashScreen) {
       LottieAnimationViewModule.forRoot();
@@ -25,19 +28,60 @@ export class Splash {
           container: document.getElementById('logoSplash'),
           loop: false,
           autoplay: true,
-          path: 'assets/animations/RollMe_Logo_NoBG.json'
-      }
+          path: 'assets/animations/RollMe_Logo_750x750.json'
+      };
 
-  }
+      setTimeout(function(){
+      $('#login').hide().delay(1000).fadeIn(1200);
+      $('#signup').hide().delay(1000).fadeIn(1200)
+      });
+  };
+
+  signupRedirect() {
+     $("#signup-form").animate({height: "toggle", opacity: "toggle"}, "slow")
+     $('#formContainer').fadeIn()
+     $('#login-form').hide()
+  };
+
+  loginRedirect() {
+     $("#login-form").animate({height: "toggle", opacity: "toggle"}, "slow")
+     $('#formContainer').fadeIn()
+     $('#signup-form').hide()
+  };
+
+  toggleLogin(ev) {
+     $("#login-form").animate({height: "toggle", opacity: "toggle"}, "slow")
+     $('#formContainer').fadeIn()
+     $('#login').fadeOut()
+     $('#signup').fadeOut()
+  };
+
+  toggleSignup() {
+     $("#signup-form").animate({height: "toggle", opacity: "toggle"}, "slow")
+     $('#formContainer').fadeIn()
+     $('#signup').fadeOut()
+     $('#login').fadeOut()
+  };
+
+  handleAnimation(anim: any) {
+      this.anim = anim;
+  };
 
   navigateToLoginPage() {
     this.navCtrl.push(LoginPage)
   }
 
+  navToHomePage(){
+    this.navCtrl.push(HomePage);
+  }
 
-  handleAnimation(anim: any) {
-      this.anim = anim;
-  };
+  login() {
+
+  }
+
+  signUp(){
+
+  }
 
 }
 
