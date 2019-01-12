587c158f1fa74974c76e3dd3eaf810447bae3d58
Brandon Lee <brandxlee11@outlook.com>
2019-01-12 13:36:27 -0500

    imported firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/home/home';
 import { Splash } from '../pages/splash/splash';
+import * as firebase from 'firebase/app';
 
 import { DataProvider } from '../providers/data/data';
+
 
 @Component({
   templateUrl: 'app.html',
