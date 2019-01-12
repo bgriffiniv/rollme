eadb5e4e8d8d2ac72bcf24ade9a47e5e05666082
Brandon Lee <brandxlee11@outlook.com>
2019-01-12 13:42:12 -0500

    added imports for fb and angular

--- src/providers/social_login/facebook_manager.ts
+++ src/providers/social_login/facebook_manager.ts
@@ -1,11 +1,17 @@
-import { Facebook } from '@ionic-native/facebook';
-import firebase from '@ionic-native/facebook';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+import { Directive, Component, NgModule } from '../../../node_modules/@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@NgModule({
+    providers: [Facebook]
+  })
 
 export class FacebookManager{
   constructor(private facebook: Facebook) {}
 
   facebookLogin(): Promise<any> {
-      return this.facebook.login(['email']);
+      return this.facebook.login(['email'])
         .then( response => {
           const facebookCredential = firebase.auth.FacebookAuthProvider
             .credential(response.authResponse.accessToken);
@@ -14,6 +20,11 @@ export class FacebookManager{
             .then( success => {
               console.log("Firebase success: " + JSON.stringify(success));
             });
-        }).catch((error) => { console.log(error) });
+        }).catch((error) => { console.log(error) })
       }
   }
+
+
+
+
+
