774020652337394f7763dbd8daeac79468bac80d
Brandon Lee <brandxlee11@outlook.com>
2019-01-12 13:48:06 -0500

    added google plus manager file

---
+++ src/providers/social_login/googleplus_manager.ts
@@ -0,0 +1,33 @@
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Directive, Component, NgModule } from '../../../node_modules/@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+
+@NgModule({
+      providers: [GooglePlus]
+})
+
+export class GooglePlusManager {
+userProfile: any = null;
+
+  constructor (private googlePlus: GooglePlus){
+    firebase.auth().onAuthStateChanged( user => {
+      if(user){
+        this.userProfile = user;
+      } else {
+          this.userProfile = null;
+      }
+    });
+  }
+
+  googleLogin(): void {
+    this.googlePlus.login({
+      'webClientId': '768651667072-o0atqb4b0o8q4u4i6n6cc8opujff9eo5.apps.googleusercontent.com',
+      'offline': true
+    }).then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
+}
