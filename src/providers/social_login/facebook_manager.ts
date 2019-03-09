179a2dc7f0a3b2353784eda84a62936d186cc6e5
Brandon Lee <brandxlee11@outlook.com>
2019-03-08 22:26:05 -0500

    added emulator files

--- src/providers/social_login/facebook_manager.ts
+++ src/providers/social_login/facebook_manager.ts
@@ -11,26 +11,52 @@ import {Events} from 'ionic-angular';
 
   @Injectable()
 export class FacebookManager{
-  constructor(private facebook: Facebook, private event:Events) {
-    this.event.subscribe('signIn_Facebook',() => {
-        console.log("Signing In...")
-        this.facebookLogin();
+isLoggedIn:boolean = false;
+users: any;
+
+constructor(private fb: Facebook) {
+  fb.getLoginStatus()
+    .then(res => {
+      console.log(res.status);
+      if(res.status === "connect") {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
     })
+    .catch(e => console.log(e));
+  }
+
+fbLogin() {
+this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then(res => {
+    if(res.status === "connected") {
+      this.isLoggedIn = true;
+      this.getUserDetail(res.authResponse.userID);
+    } else {
+      this.isLoggedIn = false;
+    }
+  })
+  .catch(e => console.log('Error logging into Facebook', e));
 }
 
-  facebookLogin(): Promise<any> {
-      return this.facebook.login(['email'])
-        .then( response => {
-          const facebookCredential = firebase.auth.FacebookAuthProvider
-            .credential(response.authResponse.accessToken);
-
-          firebase.auth().signInWithCredential(facebookCredential)
-            .then( success => {
-              console.log("Firebase success: " + JSON.stringify(success));
-            });
-        }).catch((error) => { console.log(error) })
-      }
+logout() {
+this.fb.logout()
+  .then( res => this.isLoggedIn = false)
+  .catch(e => console.log('Error logging out of Facebook', e));
   }
+
+getUserDetail(userid) {
+  this.fb.api("/"+userid+"/?fields-id,email,name,picture,gender",["public_profile"])
+    .then(res => {
+      console.log(res);
+      this.users = res;
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
+}
 
 
 
