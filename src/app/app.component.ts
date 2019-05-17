177fc5add8402b438869455f016ef13b07c84507
bgriffiniv <bgriffiniv@gmail.com>
2019-05-17 07:03:01 -0400

    First commit
    run `$ npm i -g ionic@latest` the run `$ ionic serve -lsc`

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,39 @@
-import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav, ModalController } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from '@angular/core';
 
-import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home';
-import { Splash } from '../pages/splash/splash';
-
-import { DataProvider } from '../providers/data/data';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
-  templateUrl: 'app.html',
-  providers: [DataProvider]
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
 })
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    }
+  ];
 
-
-export class MyApp {
-
-  @ViewChild(Nav) nav: Nav;
-  rootPage: any = "TabsPage";
-  data: any;
-  uid: any;
-
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, data: DataProvider,
-    modalCtrl: ModalController) {
-    console.log('Hello App Components');
-    data.init();
-    this.data = data;
-
-    /*
-    let fireBaseUser = data.auth.currentUser;
-    console.log(fireBaseUser);
-    */
-
-    this.data.auth.onAuthStateChanged((user) => {
-      console.log('Auth state changed');
-      if ( user ) {
-        this.uid = user.uid;
-        console.log(user.email + ' signed in');
-        this.nav.setRoot(HomePage, {uid: user.uid}); //I've tried also with this.rootPage = HomePage and the behavior is the same
-      } else {
-        this.uid = null;
-        console.log('no user signed in');
-        this.nav.setRoot(LoginPage);  //I've tried also with this.rootPage = LoginPage and the behavior is the same
-      }
-    });
-    console.log('Auth change callback set');
-
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-
-      let splash = modalCtrl.create(Splash);
-      splash.present();
-
-    });
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
   }
 
-  logout() {
-    console.log('Logout');
-    this.data.logout();
-    //this.nav.setRoot(LoginPage);
-  };
- };
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
