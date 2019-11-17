d2a1f5306cf552f1f4ce72212105182c630928a9
bgriffiniv <bgriffiniv@gmail.com>
2019-11-17 11:44:16 -0500

    - set up firebase service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,11 +28,11 @@ const routes: Routes = [
     loadChildren: './pages/contact/contact.module#ContactPageModule'
   },
   { path: 'link', loadChildren: './pages/link/link.module#LinkPageModule' },
-    { path: 'faq', loadChildren: './pages/faq/faq.module#FaqPageModule' },
-    { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
-    { path: 'license', loadChildren: './pages/license/license.module#LicensePageModule' },
-    { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-    { path: 'feedback', loadChildren: './pages/feedback/feedback.module#FeedbackPageModule' }
+  { path: 'faq', loadChildren: './pages/faq/faq.module#FaqPageModule' },
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'license', loadChildren: './pages/license/license.module#LicensePageModule' },
+  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
+  { path: 'feedback', loadChildren: './pages/feedback/feedback.module#FeedbackPageModule' }
 
 ];
 
