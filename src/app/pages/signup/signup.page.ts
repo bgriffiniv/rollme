bb402823f82f5138c67a0a837070ad14123ed478
Brandon Lee <brandxlee11@outlook.com>
2019-12-16 20:53:36 -0500

    Added signup page w/ test UI and routing to home page

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+    goToHomePage() {
+      this.router.navigateByUrl('/home');
+    }
 
   ngOnInit() {
   }
