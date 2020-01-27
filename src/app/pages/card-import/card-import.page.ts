91c011f1ca8871503de65ddfeae32b8b0dc2abaf
Brandon Lee <brandxlee11@outlook.com>
2020-01-27 18:30:19 -0500

    Added user flow for importing physical business card; Finalizing flow in next push

--- src/app/pages/card-import/card-import.page.ts
+++ src/app/pages/card-import/card-import.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-card-import',
@@ -10,13 +12,16 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class CardImportPage implements OnInit {
 
-  capturedCardImg: string;
+  isFrontCaptured = false;
+  isBackCaptured = false;
+
+  capturedCardImgFront: string;
+  capturedCardImgBack: string;
 
   cameraOptions: CameraOptions = {
     // Some common settings are 20, 50, and 100
     quality: 100,
     destinationType: this.camera.DestinationType.DATA_URL,
-    // In this app, dynamically set the picture source, Camera or photo gallery
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE,
     allowEdit: true,
@@ -25,19 +30,72 @@ export class CardImportPage implements OnInit {
     targetHeight: 374
   }
 
-  constructor(private route: ActivatedRoute, private router: Router, private camera: Camera) { }
+  constructor(private route: ActivatedRoute, private router: Router, private camera: Camera, private toastCtrl: ToastController) {
 
-  openCamera() {
-    this.camera.getPicture(this.cameraOptions).then((imageData) => {
-      // this.camera.DestinationType.FILE_URI gives file URI saved in local
-      // this.camera.DestinationType.DATA_URL gives base64 URI
-
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.capturedCardImg = base64Image;
-    }, (err) => {
-      console.log("Unable to obtain picture: " + err);
-    });
   }
+
+  openCameraFront() {
+        this.camera.getPicture(this.cameraOptions).then((imageData) => {
+          // this.camera.DestinationType.FILE_URI gives file URI saved in local
+          // this.camera.DestinationType.DATA_URL gives base64 URI
+
+          let base64Image = 'data:image/jpeg;base64,' + imageData;
+          this.capturedCardImgFront = base64Image;
+        }, (err) => {
+          console.log("Unable to obtain picture: " + err);
+        });
+        this.isFrontCaptured = true;
+  }
+
+   openCameraBack() {
+          this.camera.getPicture(this.cameraOptions).then((imageData) => {
+            // this.camera.DestinationType.FILE_URI gives file URI saved in local
+            // this.camera.DestinationType.DATA_URL gives base64 URI
+
+            let base64Image = 'data:image/jpeg;base64,' + imageData;
+            this.capturedCardImgBack = base64Image;
+          }, (err) => {
+            console.log("Unable to obtain picture: " + err);
+          });
+          this.isBackCaptured = true;
+   }
+
+   saveCard() {
+     this.showToast('Card saved!');
+     err => {
+       this.showToast('There was a problem saving your card :(');
+     };
+     let navigationExtras: NavigationExtras = {
+       state: {
+         data: this.capturedCardImgFront
+       }
+     };
+     this.router.navigateByUrl('/home/profile', navigationExtras);
+   }
+
+   showToast(msg) {
+       this.toastCtrl.create({
+         message: msg,
+         duration: 2000,
+         color: 'success',
+         position: 'top',
+         buttons: [
+             {
+               side: 'start',
+               icon: 'checkmark-circle',
+               handler: () => {
+                 console.log('Card saved');
+               }
+             },
+             {
+               role: 'cancel',
+               handler: () => {
+                 console.log('Cancel clicked');
+               }
+             }
+         ]
+       }).then(toast => toast.present());
+     }
 
   ngOnInit() {
   }
--- src/app/pages/home/profile/profile.page.ts
+++ src/app/pages/home/profile/profile.page.ts
@@ -4,7 +4,9 @@ import { Observable } from 'rxjs';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 import { DataService } from './../../../services/data/data.service';
-import { UserService } from './../../../services/user/user.service';
+import { UserService, User } from './../../../services/user/user.service';
+
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -13,11 +15,10 @@ import { UserService } from './../../../services/user/user.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  user;
-  keys;
-  id;
+  private users: Observable<User[]>;
 
-  capturedCardImg: string;
+  capturedCardImgFront: string;
+  isFrontCaptured = false;
 
   cameraOptions: CameraOptions = {
     // Some common settings are 20, 50, and 100
@@ -26,10 +27,16 @@ export class ProfilePage implements OnInit {
     encodingType: this.camera.EncodingType.JPEG,
     mediaType: this.camera.MediaType.PICTURE,
     allowEdit: true,
-    correctOrientation: true
+    correctOrientation: true,
+    targetWidth: 658.5,
+    targetHeight: 374
   }
 
-  constructor(private dataService: DataService, private userService: UserService, private route: ActivatedRoute, private router: Router, private camera: Camera) {
+   user;
+   keys;
+   id;
+
+  constructor(private dataService: DataService, private userService: UserService, private route: ActivatedRoute, private router: Router, private camera: Camera, public actionSheetController: ActionSheetController) {
     //console.log("Profile page started (constructor)");
 
     //this.id = this.userService.getUser();
@@ -46,18 +53,6 @@ export class ProfilePage implements OnInit {
     //});
   }
 
-  openCamera() {
-      this.camera.getPicture(this.cameraOptions).then((imageData) => {
-        // this.camera.DestinationType.FILE_URI gives file URI saved in local
-        // this.camera.DestinationType.DATA_URL gives base64 URI
-
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
-        this.capturedCardImg = base64Image;
-      }, (err) => {
-        console.log("Unable to obtain picture: " + err);
-      });
-  }
-
   goToCardImportPage() {
       this.router.navigateByUrl('/card-import');
   }
@@ -72,7 +67,52 @@ export class ProfilePage implements OnInit {
     this.router.navigateByUrl('/edit');
   }
 
+  goToUserListPage() {
+    this.router.navigateByUrl('/user-details');
+  }
+
+  async createACard() {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Create from:',
+        buttons: [{
+          text: 'Photo',
+          icon: 'images',
+          handler: () => {
+            this.router.navigateByUrl('/card-import');
+            console.log('Card import page loaded');
+          }
+        }, {
+          text: 'Template',
+          icon: 'easel',
+          handler: () => {
+            console.log('Loading template card');
+          }
+        }, {
+          text: 'New',
+          icon: 'card',
+          handler: () => {
+            console.log('Loading new card');
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }]
+      });
+      await actionSheet.present();
+  }
+
+
   ngOnInit() {
+    console.log("User List page started (init)");
+    this.users = this.userService.getUsers();
+    if (this.router.getCurrentNavigation().extras.state){
+         this.capturedCardImgFront = this.router.getCurrentNavigation().extras.state.data;
+    };
+    this.isFrontCaptured = true;
   }
 
 }
