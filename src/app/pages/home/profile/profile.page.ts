572e7e3f2f0de03a5533a3373c995980b424be5a
bgriffiniv <bgriffiniv@gmail.com>
2020-05-20 07:11:47 -0400

    ROLL-44 : Firebase Entry Init (#91)
    * ROLL-44 : Firebase Entry Init
    - use collection-add rather than doc-set for simplicity
    
    * ROLL-44 : Firebase Entry Init
    - update package for `ng2-search-filter` feature
    - fix card service method return type
    
    * ROLL-44 : Firebase Entry Init
    - use listCardsByOwner(...)
    - clean up un-used methods
    
    * ROLL-44 : Firebase Entry Init
    - clean up code
    
    * ROLL-44 : Firebase Entry Init
    - conditionally alert user to having no cards
    
    * ROLL-44 : Firebase Entry Init
    - clean up code (especially around create/update card in both profile and card-import pages)
    - remove image data being passed between pages
    - add id to update card service methods
    
    * ROLL-44 : Firebase Entry Init
    - minor changes
    
    * ROLL-44 : Firebase Entry Init
    - fix async usage in template (html)
    - conditionally display empty card list message (this can be done in many ways)
    
    * ROLL-44 : Firebase Entry Init
    - simplify card retrieval
    - no need to directly link cards: Observable<Card[]> with *ngFor="let card of cards | async" since subscribe() will continuously update cards: Card[]
    
    * ROLL-44 : Firebase Entry Init
    - fix view for card import (use the images themselves to show/hide the empty image icon)
    - add back button (maybe a cancel button later)
    
    * ROLL-44 : Firebase Entry Init
    - remove unused imports
    
    * ROLL-44 : Firebase Entry Init
    - import tap observable operator
    
    * ROLL-44 : Firebase Entry Init
    - minor changes

--- src/app/pages/home/profile/profile.page.ts
+++ src/app/pages/home/profile/profile.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
-import { UserService, User } from './../../../services/user/user.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { CardService, Card } from 'src/app/services/card/card.service';
 
 import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
+
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -15,19 +15,8 @@ import { ActionSheetController, AlertController, ToastController } from '@ionic/
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  private users: Observable<User[]>;
-  private cards: Observable<Card[]>;
 
-  id;
-  card: Card = {
-    frontImg: '',
-    backImg: ''
-  };
-  user;
-  keys;
-
-  frontImg: string;
-  isFrontCaptured = false;
+  private cards: Card[];
 
   cameraOptions: CameraOptions = {
     // Some common settings are 20, 50, and 100
@@ -41,63 +30,46 @@ export class ProfilePage implements OnInit {
     targetHeight: 374
   }
 
-  constructor(private authService: AuthService, private userService: UserService, private cardService: CardService, private activatedRoute: ActivatedRoute,
-                private router: Router, private camera: Camera, public actionSheetController: ActionSheetController, public alertController: AlertController, private toastCtrl: ToastController) {
-    console.log("Profile page started (constructor)");
-
-    console.log('Is authenticated:', this.authService.isAuthenticated());
-
-    //this.id = this.cardService.getCard();
-
-    //this.activatedRoute.data.subscribe((data) => {
-      //if (this.router.getCurrentNavigation().extras.state) {
-        //let updated = this.router.getCurrentNavigation().extras.state.data;
-        //this.cardService.setCard(this.id, updated);
-      //}
-
-      //this.card = this.cardService.getCard(this.id);
-      //this.keys = Object.keys(this.card);
-      //this.keys.splice(this.keys.indexOf("contacts"), 1);
-    //});
+  constructor(
+    private authService: AuthService,
+    private cardService: CardService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private camera: Camera,
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController,
+    private toastCtrl: ToastController
+  ) {
+    console.log('Profile Page Start');
   }
 
   ngOnInit() {
-      console.log("User List page started (init)");
+    console.log('Profile Page Init');
 
-      if (this.router.getCurrentNavigation().extras.state){
-          this.card.frontImg = this.router.getCurrentNavigation().extras.state.cardDataFront;
-          this.card.backImg = this.router.getCurrentNavigation().extras.state.cardDataBack;
-      };
+    let uid = this.authService.getCurrentUserId();
+    console.log('Current User ID: ', uid);
 
-      this.users = this.userService.listUsers();
-      this.cards = this.cardService.listCards();
-
-      this.isFrontCaptured = true;
-      this.newCardAlert();
+    this.cardService.listCardsByOwner(uid).pipe(
+      tap(data => {
+        let count = data.length;
+        if (count === 0) {
+          // alert if owned card list is empty
+          console.log('No owned cards');
+          this.newCardAlert();
+        } else {
+          // get an observable card list for the view
+          console.log('Owned cards count: ', count);
+          this.cards = data;
+        }
+      })
+    ).subscribe();
   }
 
-  goToCardImportPage() {
-      this.router.navigateByUrl('/card-import');
-  }
-
-  goToEditPage() {
-      let navigationExtras: NavigationExtras = {
-      state: {
-        data: this.user,
-        parent: 'profile'
-      }
-    };
-    this.router.navigateByUrl('/edit');
-  }
-
-  goToUserListPage() {
-    this.router.navigateByUrl('/user-details');
-  }
-
-  async createACard() {
-      const actionSheet = await this.actionSheetController.create({
-        header: 'Create from:',
-        buttons: [{
+  async createCard() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Create from:',
+      buttons: [
+        {
           text: 'Photo',
           icon: 'images',
           handler: () => {
@@ -125,53 +97,43 @@ export class ProfilePage implements OnInit {
           handler: () => {
             console.log('Cancel clicked');
           }
-        }]
-      });
-      await actionSheet.present();
+        }
+      ]
+    });
+    await actionSheet.present();
   }
 
   async newCardAlert() {
-       const alert = await this.alertController.create({
-         header: 'Welcome!',
-         subHeader: '',
-         message: 'You currently have no cards saved. Click on the card template to create one now!',
-         buttons: ['OK']
-       });
-       await alert.present();
+    const alert = await this.alertController.create({
+      header: 'Welcome!',
+      subHeader: '',
+      message: 'You currently have no cards saved. Click on the card template to create one now!',
+      buttons: ['OK']
+    });
+    await alert.present();
   }
 
-   deleteCard() {
-         console.log('card id: ' + this.card.id);
-         this.cardService.deleteCard(this.card.id).then(() => {
-           this.router.navigateByUrl('/profile');
-           this.showToast('Card deleted');
-         }, err => {
-           this.showToast('There was a problem deleting your card :(');
-         });
-   }
-
-   showToast(msg) {
-          this.toastCtrl.create({
-            message: msg,
-            duration: 2000,
-            color: 'success',
-            position: 'top',
-            buttons: [
-                {
-                  side: 'start',
-                  icon: 'checkmark-circle',
-                  handler: () => {
-                    console.log('Card saved');
-                  }
-                },
-                {
-                  role: 'cancel',
-                  handler: () => {
-                    console.log('Cancel clicked');
-                  }
-                }
-            ]
-          }).then(toast => toast.present());
-   }
-
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 2000,
+      color: 'success',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'checkmark-circle',
+          handler: () => {
+            console.log('Card saved');
+          }
+        },
+        {
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    }).then(toast => toast.present());
+  }
 }
