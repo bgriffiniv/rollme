7c228eaab3233131a9be0c53c98db1a0ae15a3a7
Brandon Lee <brandxlee11@outlook.com>
2020-05-23 15:43:30 -0400

    Minor boolean naming convention change on the exchange page

--- src/app/pages/home/exchange/exchange.page.ts
+++ src/app/pages/home/exchange/exchange.page.ts
@@ -141,7 +141,6 @@ export class ExchangePage implements OnInit {
 
   ngOnInit() {
     console.log('Exchange Page Init');
-    this.isCardFound;
     this.id = this.route.snapshot.paramMap.get('id');
 
     if (this.id) {
