aa66ae9b508587d50a5a01634a32912d33334cec
Brandon Stuart <brandxlee11@gmail.com>
2020-05-27 21:08:57 -0400

    Updated dashboard tab routes (#95)
    * Updated dashboard tab routes
    
    * Cleaned up URL paths
    
    * Cleaned up URL paths again
    
    * Added path:'home' back into home-routing.module.ts
    
    * Not sure how to address this path issue, if it even is one
    
    * ROLL-52 : URL Route Updates
    - remove redundant "/home" in linkedin link href path
    - mark root of home routing module with "home"
    - fix indentation in home routing module
    - remove tab routes from app routing module
    - leave home route in app routing module for access to tab (home) page module
    
    * ROLL-52 : URL Route Updates
    - remove root path in home routing module
    - remove unused default path (this conflicted with the default path leading to `/login` in the app routing module)
    
    Co-authored-by: Brandon Lee <brandxlee11@outlook.com>
    Co-authored-by: bgriffiniv <bgriffiniv@gmail.com>

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -5,43 +5,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
-
-{
-    path:'home',
-    component: HomePage,
-    children: [
-      {
-        path:'rolodex', loadChildren:'./rolodex/rolodex.module#RolodexPageModule'
-      },
-
-      {
-        path:'exchange', loadChildren:'./exchange/exchange.module#ExchangePageModule'
-      },
-
-      {
-        path:'profile',
-        children: [
-          {
-            path:'',
-            loadChildren:'./profile/profile.module#ProfilePageModule'
-          },
-          {
-            path:'card-import',
-            loadChildren: '../card-import/card-import.module#CardImportPageModule'
-          }
-        ]
-      },
-
-      {
-        path: '', redirectTo: 'rolodex',
-        pathMatch: 'full'
-      }
-    ]
-},
   {
-    path: '',
-    redirectTo: 'home/rolodex',
-    pathMatch: 'full'
+      path:'',
+      component: HomePage,
+      children: [
+        {
+          path:'rolodex', loadChildren:'./rolodex/rolodex.module#RolodexPageModule'
+        },
+
+        {
+          path:'exchange', loadChildren:'./exchange/exchange.module#ExchangePageModule'
+        },
+
+        {
+          path:'profile', loadChildren:'./profile/profile.module#ProfilePageModule'
+        },
+
+        {
+          path: '',
+          redirectTo: 'rolodex',
+          pathMatch: 'full'
+        }
+      ]
   }
 ];
 
