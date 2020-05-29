3b3dfcb154b8c076f61697376d3a1b07f4e5931c
bgriffiniv <bgriffiniv@gmail.com>
2020-05-28 21:09:36 -0400

    Roll 52 url route updates (#104)
    * Updated dashboard tab routes
    
    * Cleaned up URL paths
    
    * Cleaned up URL paths again
    
    * Added path:'home' back into home-routing.module.ts
    
    * Not sure how to address this path issue, if it even is one
    
    * ROLL-52 : URL Route Updates
    - remove redundant "/home" in linkedin link href path
    - mark root of home routing module with "home"
    - fix indentation in home routing module
    - remove tab routes from app routing module
    - leave home route in app routing module for access to tab (home) page module
    
    * ROLL-52 : URL Route Updates
    - remove root path in home routing module
    - remove unused default path (this conflicted with the default path leading to `/login` in the app routing module)
    
    * ROLL-52 : URL Route Updates
    - fix indentation
    
    Co-authored-by: Brandon Lee <brandxlee11@outlook.com>

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -6,27 +6,27 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-      path:'',
-      component: HomePage,
-      children: [
-        {
-          path:'rolodex', loadChildren:'./rolodex/rolodex.module#RolodexPageModule'
-        },
+    path:'',
+    component: HomePage,
+    children: [
+      {
+        path:'rolodex', loadChildren:'./rolodex/rolodex.module#RolodexPageModule'
+      },
 
-        {
-          path:'exchange', loadChildren:'./exchange/exchange.module#ExchangePageModule'
-        },
+      {
+        path:'exchange', loadChildren:'./exchange/exchange.module#ExchangePageModule'
+      },
 
-        {
-          path:'profile', loadChildren:'./profile/profile.module#ProfilePageModule'
-        },
+      {
+        path:'profile', loadChildren:'./profile/profile.module#ProfilePageModule'
+      },
 
-        {
-          path: '',
-          redirectTo: 'rolodex',
-          pathMatch: 'full'
-        }
-      ]
+      {
+        path: '',
+        redirectTo: 'rolodex',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
